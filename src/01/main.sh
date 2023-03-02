4fc644cd3c443624a266680188b718e61f62d5a0
borhez <getlink32@gmail.com>
2023-03-03 01:12:20 +0300

    ready check_args()

--- src/01/main.sh
+++ src/01/main.sh
@@ -11,4 +11,14 @@ n_files=$4
 f_letters=$5
 file_size=$6
 check_args
+#in function "check_args" are creating variables:
+    #"letters_f_ext" - list of letters for file_names
+    #"letters_f_name" - list of letters for file-extensions
+echo "in main()"
+echo $letters_f_ext
+echo $letters_f_name
+echo $file_size
+echo $n_dirs
+
+echo $(expr $n_dirs + 5)
 echo end!
