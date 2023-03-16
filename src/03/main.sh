fc9de535bd428096db7d32c4583dc7ac0829752b
borhez <getlink32@gmail.com>
2023-03-16 13:17:46 +0300

    03:start

---
+++ src/03/main.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+source ./logArg
+source ./maskArg
+source ./dateArg
+
+if [ $# -ne 1 ]
+    then echo -e 'Launch w 1 argument!\nClear by logfile: arg=1;\nClear by date: arg=2;\nClear by mask: arg=3';
+    exit 1
+fi
+
+if [ $1 == 1 ]
+    then clearByLog
+
+    elif [ $1 == 2 ]
+        then clearByDate
+
+
+    elif [ $1 == 3 ]
+        then clearByMask
+
+    else echo "Error. Wrong Argument!"; exit 1
+fi
+
+
+echo "End!"
