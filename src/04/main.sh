3eaf68c48c50434395ef2c5228c11c3db173533f
borhez <getlink32@gmail.com>
2023-03-21 20:04:12 +0300

    04:date() is changed

--- src/04/main.sh
+++ src/04/main.sh
@@ -18,19 +18,28 @@ ftCheckExistingFiles
 rm -f log*.txt
 for (( i=1; i<=2; i++))
 do
-	#Если в текущей папке уже есть файл log$i.txt, то сначала его удалю:
-	#rm -f log.txt
 	touch log$i.txt
 	nbrItem=$(($RANDOM % 901 +100))
 	echo -n "Random=$nbrItem; Creating notes in log"$i".txt; "
-	HOURS=0; MINUTES=0; SEC=0; INTERVAL=$(((60 * 60 *24) / $nbrItem))
+       	Interval=$(((60 * 60 *24) / $nbrItem))
 	while [ $nbrItem -gt 0 ]
 	do
+		ssLog=$(($ssLog + ($Interval % 60)))
+		mmLog=$(($mmLog + ($Interval / 60)))
+		if [[ $ssLog -ge 60 ]] ; then
+	                mmLog=$(($mmLog + 1))
+        	        ssLog=$(($ssLog - 60))
+        	fi
+        	if [[ $mmLog -ge 60 ]] ; then
+                	hhLog=$(($hhLog + 1))
+                	mmLog=$(($mmLog - 60))
+        	fi
+
 		echo  "$(createStringForLog)"  >> log"$i".txt
 		nbrItem=$(($nbrItem - 1))
 	done
 	echo "Created $(awk 'END {print NR}' log"$i".txt) notes!"
-		
+	ssLog=0; mmLog=0; hhLog=0		
 done
 
 
