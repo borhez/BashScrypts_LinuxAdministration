2db80adddac1f3e833ec947664c2df9b3a8b795e
borhez <getlink32@gmail.com>
2023-03-21 11:53:25 +0300

    04:userAgents!

--- src/04/main.sh
+++ src/04/main.sh
@@ -1,19 +1,33 @@
-#!/bin/bash
-source ./createStringForLog
+#!/usr/bin/bash
+source ./createStringForLog 2>/dev/null
 
+#Функция проверяет наличие требуемых файлов. При их отсутствии,
+#используя сигнал TERM, в главный процесс передаётся "exit 1"
+#Можно вызвать kill -s TERM $TOP_PID,без объявления обработчика trap "exit 1" TERM
+trap "exit 1" TERM
+TOP_PID=$$
+ftCheckExistingFiles()
+{
+	if [[ ! -f "./userAgents" || ! -f "./createStringForLog" ]] ;then
+		echo -e "There is not all files! (createsStringForLog, userAgents)\nExit"
+		kill -s TERM $TOP_PID
+	fi
+}
+
+ftCheckExistingFiles
 for (( i=1; i<=2; i++))
 do
 	#Если в текущей папке уже есть файл log$i.txt, то сначала его удалю:
 	rm -f log$i.txt
 	touch log$i.txt
 	nbrItem=$(($RANDOM % 901 +100))
-	echo -n "Random=$nbrItem; "
+	echo -n "Random=$nbrItem; Creating notes in log"$i".txt; "
 	while [ $nbrItem -gt 0 ]
 	do
 		echo  "$(createStringForLog)"  >> log"$i".txt
 		nbrItem=$(($nbrItem - 1))
 	done
-	echo "Создано $(awk 'END {print NR}' log"$i".txt) записей в log"$i".txt"
+	echo "Created $(awk 'END {print NR}' log"$i".txt) notes!"
 		
 done
 
