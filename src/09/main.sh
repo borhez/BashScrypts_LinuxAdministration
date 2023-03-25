b1aa664246e760cddd116c394438404dd0f787ed
borhez <getlink32@gmail.com>
2023-03-25 20:51:38 +0300

    09: вернул HELP/TYPE

--- src/09/main.sh
+++ src/09/main.sh
@@ -6,8 +6,12 @@ while true
 do
 	#echo "Memmory total - $(free|awk 'NR==2 {print $2}')
 	echo "
+	# HELP CPU used for %Cpu
+	# TYPE CPU gauge
 	CPU $(ft_cpu)
-	MEM_USAGE $(ft_mem)
+	# HELP MEM_USED_PERCENT is (usedMem / totalMem)%
+        # TYPE MEM_USED_PERCENT gauge
+	MEM_USED_PERCENT $(ft_mem)
 		" > /var/www/html/metrics.html
 	echo "metrics are updated"
 	sleep 7
