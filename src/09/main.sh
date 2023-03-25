df1fc09fdcb1076d8b962b92a2265bfcfa478115
borhez <getlink32@gmail.com>
2023-03-25 19:31:26 +0300

    09:cpu

--- src/09/main.sh
+++ src/09/main.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-sudo echo Start
+source collectingMetrics
+
 while true
 do
-	sudo echo "Memmory total - $(free|awk 'NR==2 {print $2}')" > /var/www/html/metrics/index.html
-	sleep 3
+	#echo "Memmory total - $(free|awk 'NR==2 {print $2}')
+	echo "
+	# HELP cpu CPU usage.
+	# TYPE cpu gauge
+	cpu $(ft_cpu)
+		" > /var/www/html/metrics.html
+	echo "metrics are updated"
+	sleep 7
 done
