1c4cc410aeeb9355438cbf3a45dd0ad13a567ba8
borhez <getlink32@gmail.com>
2023-03-25 22:06:17 +0300

    09: scrypt! configs!

--- src/09/main.sh
+++ src/09/main.sh
@@ -6,12 +6,26 @@ while true
 do
 	#echo "Memmory total - $(free|awk 'NR==2 {print $2}')
 	echo "
-	# HELP CPU used for %Cpu
+	# HELP CPU
 	# TYPE CPU gauge
 	CPU $(ft_cpu)
-	# HELP MEM_USED_PERCENT is (usedMem / totalMem)%
+
+	#|---------------| HELP MEM_USED_PERCENT
         # TYPE MEM_USED_PERCENT gauge
 	MEM_USED_PERCENT $(ft_mem)
+
+        #|---------------| HELP MEM_TOTAL_MiB
+        # TYPE MEM_TOTAL_MiB gauge
+        MEM_TOTAL_MiB $(ft_memTotal)
+
+	#|---------------| HELP DISK_USED_PERCENT
+	# TYPE DISK_USED_PERCENT gauge
+	DISK_USED_PERCENT $(ft_diskUsed)
+
+	#|---------------| HELP DISK_AVAILABLE_MiB
+	# TYPE DISK_AVAILABLE_MiB gauge
+	DISK_AVAILABLE_MiB $(ft_diskAvailable)
+
 		" > /var/www/html/metrics.html
 	echo "metrics are updated"
 	sleep 7
