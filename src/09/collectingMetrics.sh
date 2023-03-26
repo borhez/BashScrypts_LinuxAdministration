2a0aad1d1c58c58e21f9c2bb317fe87c639fd230
borhez <getlink32@gmail.com>
2023-03-27 01:37:11 +0300

    DISK_TOTAL_my; MEM_AVAIL_my

--- src/09/collectingMetrics.sh
+++ src/09/collectingMetrics.sh
@@ -5,9 +5,14 @@ ft_cpu()
 	echo $(top -b -n 1 | awk 'NR>7 {cpu=cpu+$9} END {printf("%.1f", cpu)}')
 }
 
-ft_mem()
+ft_memUsed()
 {
-	echo $(free -m | awk 'NR==2 {printf("%.1f", $3)}')
+	echo $(free -m | awk 'NR==2 {print $3}')
+}
+
+ft_memAvail()
+{
+	echo $(free -m | awk 'NR==2 {print $7}')
 }
 
 ft_memTotal()
@@ -26,13 +31,23 @@ ft_diskAvailable()
 	echo $(df -m / | awk 'NR==2 {print $4}')
 }
 
+ft_diskTotal()
+{
+	echo $(df -m / | awk 'NR==2 {print$2}')
+}
+
 echo \# HELP CPU_my
 echo \# TYPE CPU_my gauge
 echo CPU_my $(ft_cpu)
 
 echo \# HELP MEM_USED_my
 echo \# TYPE MEM_USED_my gauge
-echo MEM_USED_my $(ft_mem)
+echo MEM_USED_my $(ft_memUsed)
+
+echo \# HELP MEM_AVAIL_my
+echo \# TYPE MEM_AVAIL_my gauge
+echo MEM_AVAIL_my $(ft_memAvail)
+
 
 echo \# HELP MEM_TOTAL_my
 echo \# TYPE MEM_TOTAL_my gauge
@@ -45,6 +60,10 @@ echo DISK_USED_my $(ft_diskUsed)
 echo \# HELP DISK_AVAILABLE_my
 echo \# TYPE DISK_AVAILABLE_my gauge
 echo DISK_AVAILABLE_my $(ft_diskAvailable)
+
+echo \# HELP DISK_TOTAL_my
+echo \# TYPE DISK_TOTAL_my gauge
+echo DISK_TOTAL_my $(ft_diskTotal)
 
 echo \# HELP DISK_READ_my
 echo \# TYPE DISK_READ_my gauge
